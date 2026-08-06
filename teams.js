84c573e903dd9b2d31a1dff33b29b94e796d62f2
MrAxeman777 <arsharma892@gmail.com>
2026-08-06 09:10:52 -0500

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,1 +1,49 @@
+const teams = [
+"Arsenal",
+"Manchester City",
+"Manchester United",
+"Aston Villa",
+"Liverpool",
 
+"Barcelona",
+"Real Madrid",
+"Villarreal",
+"Atletico Madrid",
+"Real Betis",
+
+"Inter Milan",
+"Napoli",
+"AS Roma",
+"Como",
+
+"Bayern Munich",
+"Borussia Dortmund",
+"RB Leipzig",
+"VfB Stuttgart",
+
+"Paris Saint-Germain",
+"RC Lens",
+"LOSC Lille",
+"Olympique Lyonnais",
+
+"PSV Eindhoven",
+"Feyenoord",
+
+"FC Porto",
+"Sporting CP",
+
+"Club Brugge",
+"Slavia Prague",
+
+"Galatasaray",
+"Fenerbahçe",
+
+"Shakhtar Donetsk",
+"GNK Dinamo Zagreb",
+
+"Olympiacos",
+"Red Star Belgrade",
+
+"Celtic",
+"AGF Aarhus"
+];
